e7e53b283a41b45e4a4f73f8999d91ee62cbe6d4
Merus23 <mateus.satira@gmail.com>
2023-10-18 15:25:04 -0300

    feat: custumize the dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import styles from './Dashboard.module.css'
+
+import { Link } from 'react-router-dom'
+import {useAuthValue} from '../../context/AuthContext'
+import {useFetchDocuments} from '../../hooks/useFetchDocuments'
+
+
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+  
+  const post = []
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {post && post.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className='btn'>Criar seu primeiro posts</Link>
+        </div>
+      ) : (
+        <div>
+          Tem post!
+        </div>
+      )}
     </div>
   )
 }
