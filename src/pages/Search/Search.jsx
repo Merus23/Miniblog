7a7f9cffb4f7e1389fe154bd186517becfc95db8
Merus23 <mateus.satira@gmail.com>
2023-10-18 14:49:19 -0300

    feat: finish Search components

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from 'react';
+import styles from './Search.module.css';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useQuery } from '../../hooks/useQuery';
 
-import { useFetchDocuments } from '../../hooks/useFetchDocuments'
-import { useQuery } from '../../hooks/useQuery'
+import PostDetail from '../../components/PostDetail'
+import { Link } from 'react-router-dom';
 
 const Search = () => {
     const query = useQuery();
     const search = query.get("q");
   
+
+    const {documents: posts} = useFetchDocuments('posts', search)
+
     return (
-        <div>
-        <h1>Search</h1>
-        <p>aa</p>
+        <div className={styles.search_container}>
+            <h1>Search</h1>
+            <div>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Nenhum post encontrado...</p>
+                        <Link to="/" className="btn btn-dark">Voltar</Link>
+                    </div>
+                )}
+                {posts && posts.map((post) => (
+                    <PostDetail key={post.id} post={post} />
+                ))}
+            </div>
+        
         </div>
     )
 }
