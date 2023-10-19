c9931a4dabdd14fdb047325996953d242cba446c
Merus23 <mateus.satira@gmail.com>
2023-10-19 15:08:02 -0300

    fix: fix useFetchDocuments loop bug; feat: create some features on Dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,13 +10,13 @@ const Dashboard = () => {
   const {user} = useAuthValue()
   const uid = user.uid
   
-  const post = []
+  const {documents: posts, loading} = useFetchDocuments('posts', null,uid)
 
   return (
     <div>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
-      {post && post.length === 0 ? (
+      {posts && posts.length === 0 ? (
         <div className={styles.noposts}>
           <p>Não foram encontrados posts</p>
           <Link to="/posts/create" className='btn'>Criar seu primeiro posts</Link>
@@ -26,6 +26,8 @@ const Dashboard = () => {
           Tem post!
         </div>
       )}
+
+      {posts && posts.map((post) => <h3 key={post.id}>{post.title}</h3>)}
     </div>
   )
 }
